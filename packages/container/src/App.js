8b2006f7d57793ae18fea00696845b05498a086a
tombru <tomasbruhn@gmail.com>
2022-03-11 13:27:38 +0100

    Add github action

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {BrowserRouter} from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header"
 
-export default  () => {
-    return <div>
-        <h1>Hi there again!!!</h1>
-        <hr />
-        <MarketingApp />
-    </div>
+export default () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <MarketingApp/>
+            </div>
+        </BrowserRouter>
+    )
 }
